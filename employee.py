535871637bb63c209d8bed0a49867ae7644d71da
Jukes, Alfred (k21064463) <alfred.jukes@kcl.ac.uk>
2022-11-03 17:56:50 +0000

    removed local tests

--- employee.py
+++ employee.py
@@ -36,25 +36,13 @@ class Employee:
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = Employee('Billie', salary=4000)
-print(billie.get_pay())
-print(str(billie))
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
 charlie = Employee('Charlie', hoursContracted=100, payPerHour=25)
-print(charlie.get_pay())
-print(str(charlie))
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
 renee = Employee('Renee', salary=3000, commission=200, contractsCompleted=4)
-print(renee.get_pay())
-print(str(renee))
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
 jan = Employee('Jan', hoursContracted=150, payPerHour=25, commission=220, contractsCompleted=3)
-print(jan.get_pay())
-print(str(jan))
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = Employee('Robbie', salary=2000, bonus=1500)
-print(robbie.get_pay())
-print(str(robbie))
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = Employee('Ariel', bonus=600, hoursContracted=120, payPerHour=30)
-print(ariel.get_pay())
-print(str(ariel))
